b6bd7af07203b0c65ace98ced245923799c94126
chameleon001 <chameleon001@naver.com>
2018-01-23 20:23:43 +0900

    20180123 dp시작

---
+++ opencv_prat/11004.c
@@ -0,0 +1,45 @@
+/*
+수 N개 A1, A2, ..., AN이 주어진다. A를 오름차순 정렬했을 때, 앞에서부터 K번째 있는 수를 구하는 프로그램을 작성하시오.
+입력
+
+첫째 줄에 N(1 ≤ N ≤ 5,000,000)과 K (1 ≤ K ≤ N)이 주어진다.
+
+둘째에는 A1, A2, ..., AN이 주어진다. (-109 ≤ Ai ≤ 109)
+출력
+
+A를 정렬했을 때, 앞에서부터 K번째 있는 수를 출력한다.
+예제 입력
+
+5 2
+4 1 2 3 5
+
+예제 출력
+
+2
+*/
+#include <iostream>
+
+#include <algorithm>
+
+#include <cstdio>
+
+using namespace std;
+
+int main()
+
+{
+
+	int n, k, num[5000000];
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; ++i)
+
+		scanf("%d", &num[i]);
+
+	sort(num, num + n);
+
+	cout << num[k - 1];
+
+}
+
